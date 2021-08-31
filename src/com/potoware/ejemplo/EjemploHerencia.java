be9150de17ac873cd8539418645a05795c90af18
Alejandro Potosi <alejandropotosi@outlook.com>
2021-08-30 22:05:23 -0500

    Creacion de clase especializada Padre.Hija.Hija

--- src/com/potoware/ejemplo/EjemploHerencia.java
+++ src/com/potoware/ejemplo/EjemploHerencia.java
@@ -7,6 +7,21 @@ public class EjemploHerencia {
         alumno.setNombre("Alejandro");
         alumno.setApellido("Potosi");
         alumno.setInstitucion("Uninpahu");
+        alumno.setNotaEspaniol(5.5);
+        alumno.setNotaHistoria(5.2);
+        alumno.setNotaMatematicas(2.1);
+
+        AlumnoInternacional alumnoInternacional = new AlumnoInternacional();
+        alumnoInternacional.setNombre("Peter");
+        alumnoInternacional.setApellido("Parker");
+        alumnoInternacional.setPais("Mexico");
+        alumnoInternacional.setEdad(15);
+        alumnoInternacional.setInstitucion("Cambridge University");
+        alumnoInternacional.setNotaIdiomas(5.0);
+        alumnoInternacional.setNotaEspaniol(5.8);
+        alumnoInternacional.setNotaMatematicas(5.3);
+        alumnoInternacional.setNotaHistoria(5.4);
+
 
         Profesor profesor = new Profesor();
         profesor.setNombre("Juan");
@@ -19,5 +34,15 @@ public class EjemploHerencia {
                 " - Asignatura: "+profesor.getAsignatura());
 
         System.out.println("alumno = " + ((Alumno) alumno).getInstitucion());
+
+        System.out.println(alumnoInternacional.getNombre()+" "+alumnoInternacional.getApellido());
+        System.out.println("Pais = " + alumnoInternacional.getPais());
+        Class clase = alumnoInternacional.getClass();
+        while (clase.getSuperclass()!= null){
+            String hija = clase.getName();
+            String padre = clase.getSuperclass().getName();
+            System.out.println(hija + " es clase hija de la clase padre "+padre);
+            clase = clase.getSuperclass();
+        }
     }
 }
