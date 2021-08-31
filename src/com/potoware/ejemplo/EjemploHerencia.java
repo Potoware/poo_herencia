c91721646330827753c18845493d7b70b575a869
Alejandro Potosi <alejandropotosi@outlook.com>
2021-08-30 21:52:54 -0500

    se implementaron pruebas de instanciar Objetos de tipo padre

--- src/com/potoware/ejemplo/EjemploHerencia.java
+++ src/com/potoware/ejemplo/EjemploHerencia.java
@@ -1,10 +1,12 @@
-package com.potoware.pooherencia;
+package com.potoware.ejemplo;
+import com.potoware.pooherencia.*;
 
 public class EjemploHerencia {
     public static void main(String[] args) {
         Alumno alumno = new Alumno();
         alumno.setNombre("Alejandro");
         alumno.setApellido("Potosi");
+        alumno.setInstitucion("Uninpahu");
 
         Profesor profesor = new Profesor();
         profesor.setNombre("Juan");
@@ -15,5 +17,7 @@ public class EjemploHerencia {
         System.out.println("Profesor: "+
                 profesor.getNombre()+" "+profesor.getApellido()+
                 " - Asignatura: "+profesor.getAsignatura());
+
+        System.out.println("alumno = " + ((Alumno) alumno).getInstitucion());
     }
 }
